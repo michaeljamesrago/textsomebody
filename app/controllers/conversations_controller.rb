82eb2eb6af6ec39872dd1d88174988fdac248c02
Michael Rago <michael.rago@gmail.com>
2023-01-02 13:15:11 -0500

    Create conversation controller and views

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,24 @@
+class ConversationsController < ApplicationController
+  def new
+    @conversation = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    if @conversation.save
+      redirect_to @conversation
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @conversation = Conversation.find(params[:id])
+  end
+
+  private
+
+    def conversation_params
+      params.require(:conversation).permit(:number)
+    end
+end
