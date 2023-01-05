f82b78b2def82dbc2891f462441d51b761e8f96e
Michael Rago <michael.rago@gmail.com>
2023-01-05 17:59:52 -0500

    Improve styling and add stimulus to clear text field

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,11 +3,13 @@ class Conversation < ApplicationRecord
   has_many :messages
 
   def send_sms(body)
-    client = Twilio::REST::Client.new
-    client.messages.create(
-      from: ENV['TWILIO_FROM_NUMBER'],
-      body: body,
-      to: number
-    )
+    if ENV["RAILS_ENV"] != "development"
+      client = Twilio::REST::Client.new
+      client.messages.create(
+        from: ENV['TWILIO_FROM_NUMBER'],
+        body: body,
+        to: number
+      )
+    end
   end
 end
