f700cc00e7237cc2d82d169402e5463c9d21d38d
Michael Rago <michael.rago@gmail.com>
2023-01-15 18:40:06 -0500

    Add static pages about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :conversations do
-    resources :messages
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  resources :conversations, only: [:new, :create, :show] do
+    post '/messages', to: "messages#create"
   end
+
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
   
   root 'conversations#new'
 
