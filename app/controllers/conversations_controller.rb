099a1ce8868e101962e2d293cd0919608389eb3e
Michael Rago <michael.rago@gmail.com>
2023-01-18 16:34:32 -0500

    Use session coookie in conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,11 +1,16 @@
 class ConversationsController < ApplicationController
   def new
-    @conversation = Conversation.new
+    if !!session[:conversation_id]
+      redirect_to conversation_path(session[:conversation_id])
+    else
+      @conversation = Conversation.new
+    end
   end
 
   def create
     @conversation = Conversation.new(conversation_params)
     if @conversation.save
+      session[:conversation_id] = @conversation.id
       redirect_to @conversation
     else
       render 'new', status: :unprocessable_entity
